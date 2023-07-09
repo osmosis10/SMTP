0a550b3d3f15ec394e3af24af6364d162a03829d
conlan2 <conlan2@gmail.com>
2023-07-09 12:41:45 -0600

    Created the initial stages of creating the public cipher, the username and the password for the connection protocol

--- client.py
+++ client.py
@@ -1,6 +1,27 @@
 # This is an example from "Computer Networking: A Top Down Approach" textbook chapter 2
 import socket
 import sys
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+def initial_connection_protocol(clientSocket):
+    # This is the server client communication protocol for when the initial connection
+
+    # Creates the key and cipher from the server public key
+    f = open("server_public.pem", "rb")
+    server_pub = RSA.importKey(f.read())
+    f.close()
+    cipher_rsa_server = PKCS1_OAEP.new(server_pub)
+
+    # Enters and sends the username
+    username = input("Enter Username:")
+    encrypted = cipher_rsa_server.encrypt(username.encode("ascii"))
+    clientSocket.send(encrypted)
+
+    # Enters and sends the password
+    password = input("Enter Password:")
+    encrypted = cipher_rsa_server.encrypt(password.encode("ascii"))
+    clientSocket.send(encrypted)
+
 
 def client():
     # Server Information
@@ -18,16 +39,7 @@ def client():
         #Client connect with the server
         clientSocket.connect((serverName,serverPort))
         
-        # Client receives a message and send it to the client
-        message = clientSocket.recv(2048).decode('ascii')
-        
-        #Client send message to the server
-        message = input(message).encode('ascii')
-        clientSocket.send(message)
-        
-        # Client receives a message from the server and print it
-        message = clientSocket.recv(2048)
-        print(message.decode('ascii'))
+        initial_connection_protocol(clientSocket)
         
         # Client terminate connection with the server
         clientSocket.close()
