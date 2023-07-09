e38520d34e855ffbc900844aae29cbdb5ba37115
conlan2 <conlan2@gmail.com>
2023-07-09 13:33:58 -0600

    Added client side for recieving the (or being rejected by) the sym key

--- client.py
+++ client.py
@@ -3,6 +3,7 @@ import socket
 import sys
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
+from Crypto.Cipher import AES
 def initial_connection_protocol(clientSocket):
     # This is the server client communication protocol for when the initial connection
 
@@ -21,6 +22,32 @@ def initial_connection_protocol(clientSocket):
     password = input("Enter Password:")
     encrypted = cipher_rsa_server.encrypt(password.encode("ascii"))
     clientSocket.send(encrypted)
+
+    # Receives the Response
+    response = clientSocket.recv(2048)
+    if response.decode('ascii') == "Invalid Username or Password":
+        print("Invalid Username or Password\nTerminating")
+        return False, None
+    else:
+        # Creates the public key for the client and cipher from the client public key
+        f = open(f"{username}_private.pem", "rb")
+        client_pri = RSA.importKey(f.read())
+        f.close()
+        private_rsa_client = PKCS1_OAEP.new(client_pri)
+
+        # Receives the symmetric key from the server
+        sym_key = private_rsa_client.decrypt(response).decode("ascii")
+        # noinspection PyTypeChecker
+        sym_cipher = AES.new(sym_key, AES.MODE_ECB)
+
+        # Sends the OK to the server
+        encrypted = sym_cipher.encrypt("OK".encode("ascii"))
+        clientSocket.send(encrypted)
+
+        return True, sym_key
+
+
+
 
 
 def client():
