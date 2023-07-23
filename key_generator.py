6aee1f6500db12918361eb0ba3a27fdad535946f
conlan2 <conlan2@gmail.com>
2023-07-23 12:47:43 -0600

    Added key_generator.py file and a fix for a crash when folders had only one date

---
+++ key_generator.py
@@ -0,0 +1,48 @@
+
+from Crypto.PublicKey import RSA
+
+# Use this to generate RSA public and private keys
+def generate_keys():
+    private_key = RSA.generate(2048)
+    public_key = private_key.public_key()
+    return private_key, public_key
+
+
+
+
+
+# You can use this to print out the public and private keys
+# The commented out parts will print out the modulus and exponent
+# components of the key (n=modulus, e,d = exponent)
+def print_keys(private_key, public_key):
+    # Print out the modulus of the private key
+    # print(f'Start private_key.n {private_key.n} Stop private_key.n\n')
+    # Print out the exponent of the private key
+    # print(f'Start private_key.d {private_key.d} Stop private_key.d\n')
+    # Print out the modulus of the public key
+    # print(f'Start public_key.n {public_key.n} Stop public_key.n\n')
+    # Print out the exponent of the public key
+    # print(f'Start public_key.e {public_key.e} Stop public_key.e\n')
+
+    private_key_pem = private_key.export_key()
+    public_key_pem = public_key.export_key()
+
+    print(private_key_pem)
+    print(public_key_pem)
+    return
+
+
+# Use this to save the server private key as a pem file to the current
+# directory
+def export_private_key(private_key):
+    with open(f'server_private.pem', 'wb') as file:
+        file = file.write(private_key.export_key('PEM'))
+    return
+
+
+# Use this to save the server public key as a pem file to the current
+# directory
+def export_public_key(public_key):
+    with open(f'server_public.pem', 'wb') as file:
+        file = file.write(public_key.export_key('PEM'))
+    return
--- server/server.py
+++ server/server.py
@@ -11,55 +11,10 @@ from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import pad, unpad
 
 
-# Use this to generate RSA public and private keys
-def generate_keys():
-    private_key = RSA.generate(2048)
-    public_key = private_key.public_key()
-    return private_key, public_key
-
-
 # Use this to generate a sym_key of size 32(256bits)
 # Can change size to change the size of the key
 def generate_sym_key(size=32):
     return get_random_bytes(size)
-
-
-# You can use this to print out the public and private keys
-# The commented out parts will print out the modulus and exponent
-# components of the key (n=modulus, e,d = exponent)
-def print_keys(private_key, public_key):
-    # Print out the modulus of the private key
-    # print(f'Start private_key.n {private_key.n} Stop private_key.n\n')
-    # Print out the exponent of the private key
-    # print(f'Start private_key.d {private_key.d} Stop private_key.d\n')
-    # Print out the modulus of the public key
-    # print(f'Start public_key.n {public_key.n} Stop public_key.n\n')
-    # Print out the exponent of the public key
-    # print(f'Start public_key.e {public_key.e} Stop public_key.e\n')
-
-    private_key_pem = private_key.export_key()
-    public_key_pem = public_key.export_key()
-
-    print(private_key_pem)
-    print(public_key_pem)
-    return
-
-
-# Use this to save the server private key as a pem file to the current
-# directory
-def export_private_key(private_key):
-    with open(f'server_private.pem', 'wb') as file:
-        file = file.write(private_key.export_key('PEM'))
-    return
-
-
-# Use this to save the server public key as a pem file to the current
-# directory
-def export_public_key(public_key):
-    with open(f'server_public.pem', 'wb') as file:
-        file = file.write(public_key.export_key('PEM'))
-    return
-
 
 # User this to save the server public key binary to a variable
 def import_public_key(username):
@@ -171,6 +126,10 @@ def substring(string, delimiter):
 
 # Bubble sort for dates
 def bubblesort(elements):
+    # Added to prevent issues when the length of elements is 1 (otherwise
+    # returns 1) - Conlan
+    if len(elements) == 1:
+        return elements
     swapped = False
     for n in range(len(elements)-1, 0, -1):
         for i in range(n):
